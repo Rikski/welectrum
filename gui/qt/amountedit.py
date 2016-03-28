374e5dbe349f669a936792365fe3d339777f9aba
Rikski <hendrik-richter@freenet.de>
2016-03-28 09:37:52 +0200

    Edit units

--- gui/qt/amountedit.py
+++ gui/qt/amountedit.py
@@ -78,11 +78,11 @@ class BTCAmountEdit(AmountEdit):
         p = self.decimal_point()
         assert p in [2, 5, 8]
         if p == 8:
-            return 'BTC'
+            return 'WLC'
         if p == 5:
-            return 'mBTC'
+            return 'mWLC'
         if p == 2:
-            return 'bits'
+            return 'µWLC'
         raise Exception('Unknown base unit')
 
     def get_amount(self):
